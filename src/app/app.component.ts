dbe3e477f309a5f6a1388ca4402fa6527f37eb9b
Mahesh <Mahesh9785>
2023-01-27 19:19:01 +0530

    Practice on Sharing data between child and parent components, Use of Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  title="Practice for pipes";
+
+  date=new Date;
+
+  person={
+    name:'Mahesh',
+    age:20
+  }
+
   items:string[]=[]; //items is the array of string values that are collected from the child component (First Component)
 
   //addItem function simply adds new items to the items array.
